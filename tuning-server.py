920465eb6fa9d5efb57c929ef760d67c88eb95c9
Andrew Klepchick <aklepchi@amazon.com>
2025-11-12 10:56:37 -0800

    Made compatible with osb vectorsearch
    Signed-off-by: Andrew Klepchick <aklepchi@amazon.com>

--- tuning-server.py
+++ tuning-server.py
@@ -3,10 +3,24 @@ import subprocess
 import os
 import opensearchpy as opensearch
 import json
+import tempfile
 
 app = Flask(__name__)
 
 CONFIG = json.load(open('config.json'))
+
+def generate_workload_params(params_file, extra_params=None):
+    if extra_params is None or len(extra_params) == 0:
+        return params_file
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        params = json.load(open(params_file))
+        for k, v in extra_params.items():
+            params[k] = v
+        f.write(json.dumps(params))
+        f.flush()
+        os.fsync(f.fileno())
+        print(f.name)
+        return f.name
 
 class OSCluster:
     def __init__(self, url):
@@ -15,20 +29,33 @@ class OSCluster:
         self.index_built = False
 
     def create_command(self):
-        if self.index_built == False:
-            return [
-                'opensearch-benchmark',
-                'run',
-                f'--workload={CONFIG["workload"]}',
-                '--test-mode',
-                '--results-format=csv',
-                '--kill-running-processes',
-                '--results-file=results.csv'
-            ]
-        else:
-            return []
+        # cmd = [
+        #     'opensearch-benchmark',
+        #     'run',
+        #     f'--workload={CONFIG["workload"]}',
+        #     '--test-mode',
+        #     '--results-format=csv',
+        #     '--kill-running-processes',
+        #     '--results-file=results.csv'
+        # ]
+        params_file = generate_workload_params(CONFIG["workload-params"], CONFIG["extra-workload-params"])
+        params = open(params_file).read()
+        cmd = [
+            'opensearch-benchmark',
+            'run',
+            f'--target-hosts={CONFIG["cluster-url"]}',
+            f'--workload={CONFIG["workload"]}',
+            f'--workload-params={params}',
+            '--pipeline=benchmark-only',
+            '--test-procedure=no-train-test',
+            '--kill-running-processes',
+            '--results-file=results.csv'
+        ]
+        return cmd
 
     def run_osb(self):
+        if os.path.exists('results.csv'):
+            os.remove('results.csv')
         result = subprocess.run(self.create_command())
 
 CLUSTER = OSCluster(CONFIG["cluster-url"])
@@ -41,6 +68,21 @@ def run_osb():
     print(result)
     # Send the result file back
     return send_file('results.csv')
+
+@app.route('/send_opensearch', methods=['POST'])
+def send_opensearch():
+    # Get the file from the request
+    file = request.files['file']
+    # Save the file
+    file.save('kNN.tar.gz')
+
+    os.remove('kNN') if os.path.exists('kNN') else None
+    # Untar the file
+    subprocess.run(['tar', '-xzf', 'kNN.tar.gz'])
+    subprocess.run(['rm', 'kNN.tar.gz'])
+    subprocess.run(['./gradlew', 'run'], cwd='kNN')
+    # Send the success response
+    return 'Success'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
